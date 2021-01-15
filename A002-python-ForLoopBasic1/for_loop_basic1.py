1ba1af5640aa6480eded1195cc7780a98c06e482
Jansen Hillis <jansen.hillis@outlook.com>
2021-01-14 23:20:30 -0800

    minor corrections

--- A002-python-ForLoopBasic1/for_loop_basic1.py
+++ A002-python-ForLoopBasic1/for_loop_basic1.py
@@ -3,12 +3,12 @@ for i in range(151):
     print(i)
 
 #2. Multiple of Five - print all multiples of 5 from 5 to 1000
-for i in range(5, 1005, 5):
+for i in range(5, 1001, 5):
     print(i)
 
 #3. Counting, the Dojo Way - print integers 1 to 100. If divisible by 5, print "Coding" instead. 
 # If divisible by 10, print "Coding Dojo"
-for i in range(1, 101, 1):
+for i in range(1, 101):
     if i % 10 == 0:
         print("Coding Dojo")
     elif i % 5 == 0:
@@ -24,7 +24,7 @@ for i in range(500001):
 print("Sum: " + str(sum))
 
 #5. Countdown by Fours - print positive numbers starting at 2018, counting down by 4
-for i in range(2019, 0, -4):
+for i in range(2018, 0, -4):
     print(i)
 
 #6. Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum, to highNum, 
@@ -32,7 +32,7 @@ for i in range(2019, 0, -4):
 lowNum = 0
 highNum = 10
 mult = 3
-for i in range(lowNum, highNum, mult):
-    if (i % mult == 0): #integers that are multiples of mult
+for i in range(lowNum, highNum + 1):
+    if i % mult == 0: #integers that are multiples of mult
         print(i)
 
