48f26bbda6c6c7c9777b7866297008c6c4da7cb5
Jansen Hillis <jansen.hillis@outlook.com>
2021-01-15 20:28:57 -0800

    6 complete

--- A005-python-ForLoopBasic2/forLoopBasic2.py
+++ A005-python-ForLoopBasic2/forLoopBasic2.py
@@ -24,29 +24,63 @@ def count_positives(lst):
     lst[len(lst) - 1] = numPos
     return lst
 
-print(count_positives([-1, 1, 1, 1]))
-print(count_positives([1,6,-4,-2,-7,-2]))
-print(count_positives([1, 1]))
-print(count_positives([1]))
+# print(count_positives([-1, 1, 1, 1]))
+# print(count_positives([1,6,-4,-2,-7,-2]))
+# print(count_positives([1, 1]))
+# print(count_positives([1]))
 
 # Sum Total - Create a function that takes a list and returns the sum of all the values in the array.
 # Example: sum_total([1,2,3,4]) should return 10
 # Example: sum_total([6,3,-2]) should return 7
+def sum_total(lst):
+    totalSum = 0
 
+    for item in lst:
+        totalSum += item
+    
+    return totalSum
+
+# print(sum_total([1,2,3,4]))
+# print(sum_total([6,3,-2]))
 
 # Average - Create a function that takes a list and returns the average of all the values.
 # Example: average([1,2,3,4]) should return 2.5
+def average(lst):
+    totalSum = 0
 
+    for item in lst:
+        totalSum += item
+    
+    return totalSum / len(lst)
+
+# print(average([1,2,3,4]))
 
 # Length - Create a function that takes a list and returns the length of the list.
 # Example: length([37,2,1,-9]) should return 4
 # Example: length([]) should return 0
+def length(lst):
+    return len(lst)
 
+# print(length([37, 2, 1, -9]))
+# print(length([]))
 
-# Minimum - Create a function that takes a list of numbers and returns the minimum value in the list. If the list is empty, have the function return False.
+# Minimum - Create a function that takes a list of numbers and returns the minimum value in the list. 
+# If the list is empty, have the function return False.
 # Example: minimum([37,2,1,-9]) should return -9
 # Example: minimum([]) should return False
+def minimum(lst):
+    if not lst:
+        return False
 
+    minVal = lst[0]
+    for item in lst:
+        if minVal > item:
+            minVal = item
+    
+    return minVal
+
+print(minimum([37, 2, 1, -9]))
+print(minimum([]))
 
 # Maximum - Create a function that takes a list and returns the maximum value in the array. If the list is empty, have the function return False.
 # Example: maximum([37,2,1,-9]) should return 37
