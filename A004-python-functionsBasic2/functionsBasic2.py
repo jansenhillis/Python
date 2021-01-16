42fc98095987fa2ca8d91a6c1a803a7ad44a2371
Jansen Hillis <jansen.hillis@outlook.com>
2021-01-15 19:40:37 -0800

    Problem 5 complete

--- A004-python-functionsBasic2/functionsBasic2.py
+++ A004-python-functionsBasic2/functionsBasic2.py
@@ -45,6 +45,18 @@ def values_greater_than_second(lst):
 # print (values_greater_than_second([5]))
 # print (values_greater_than_second([5, 2]))
 
-# 5. This Length, That Value - Write a function that accepts two integers as parameters: size and value. The function should create and return a list whose length is equal to the given size, and whose values are all the given value.
+# 5. This Length, That Value - Write a function that accepts two integers as parameters: size and value. 
+# The function should create and return a list whose length is equal to the given size, and 
+# whose values are all the given value.
 # Example: length_and_value(4,7) should return [7,7,7,7]
 # Example: length_and_value(6,2) should return [2,2,2,2,2,2]
+def length_and_value(size, value):
+    newList = []
+
+    for i in range(size):
+        newList.append(value)
+
+    return newList
+
+# print(length_and_value(4, 7))
+# print(length_and_value(6, 2))
