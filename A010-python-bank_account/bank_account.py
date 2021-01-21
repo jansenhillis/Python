61ad1d6858c3534341d2ed1c00bf260a16dd319a
Jansen Hillis <jansen.hillis@outlook.com>
2021-01-20 17:24:35 -0800

    intial commit

---
+++ A010-python-bank_account/bank_account.py
@@ -0,0 +1,12 @@
+class BankAccount:
+	def __init__(self, int_rate, balance): # don't forget to add some default values for these parameters!
+		# your code here! (remember, this is where we specify the attributes for our class)
+                # don't worry about user info here; we'll involve the User class sooncopy
+	def deposit(self, amount):
+		# your code here
+	def withdraw(self, amount):
+		# your code here
+	def display_account_info(self):
+		# your code here
+	def yield_interest(self):
+		# your code 
