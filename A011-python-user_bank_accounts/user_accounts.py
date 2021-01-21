093203d039141be6f133e727d9c04b72bcab3db9
Jansen Hillis <jansen.hillis@outlook.com>
2021-01-20 22:04:09 -0800

    added minor tweaks, muliple accounts per user support

--- A011-python-user_bank_accounts/user_accounts.py
+++ A011-python-user_bank_accounts/user_accounts.py
@@ -1,8 +1,9 @@
 class BankAccount:
-    def __init__(self, int_rate = 0, balance = 0):
+    def __init__(self, name, int_rate = 0, balance = 0):
+        self.name = name
         self.int_rate = int_rate
         self.balance = balance
-
+        
     def deposit(self, amount):
         if amount < 0:
             print("Cannot deposit that amount: $", amount)
@@ -19,12 +20,11 @@ class BankAccount:
             self.balance -= 5
             return self
         
-        #withdraw from the account
         self.balance -= amount
         return self
 
     def display_account_info(self):
-        print(f"Account Balace: ${self.balance}, Interest Rate: {self.int_rate}%")
+        print(f"Account Name: {self.name} \nBalance: ${self.balance}, Interest Rate: {self.int_rate}%")
         return self
 
     def yield_interest(self):
@@ -34,16 +34,18 @@ class BankAccount:
 class User:
     accounts = []
 
-    def __init__(self, name, email, accounts):
+    def __init__(self, name, email):
         self.name = name
         self.email = email
 
-    def create_account(new_account):
-        accounts.append(new_account)
-        print(accounts)
+    def create_account(self, new_account):
+        self.accounts.append(new_account)
 
     def display_user_balance(self):
-        print(f"User: {self.name}, Balance: ${self.account.balance}")
+        print(f"Accounts for user: {self.name}")
+        for account in self.accounts:
+            account.display_account_info()
+
         return self
 
     def make_deposit(self, amount):
@@ -65,5 +67,8 @@ class User:
         other_user.account.balance += amount
         return self
 
-user1 = User("User 1", "user@1.com", BankAccount(0.05, 1000))
+user1 = User("User 1", "user@1.com")
+user1.create_account(BankAccount(name = "checking", int_rate=0.1, balance=9000))
+user1.create_account(BankAccount(name = "savings", int_rate=0.05, balance=10))
+user1.create_account(BankAccount(name = "retirement", balance=100000))
 user1.display_user_balance()
