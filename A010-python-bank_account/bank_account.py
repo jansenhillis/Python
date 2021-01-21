20c8679b8465cb4a2a7de273565525adef1d98bf
Jansen Hillis <jansen.hillis@outlook.com>
2021-01-20 21:06:34 -0800

    assignment complete

--- A010-python-bank_account/bank_account.py
+++ A010-python-bank_account/bank_account.py
@@ -1,12 +1,38 @@
 class BankAccount:
-	def __init__(self, int_rate, balance): # don't forget to add some default values for these parameters!
-		# your code here! (remember, this is where we specify the attributes for our class)
-                # don't worry about user info here; we'll involve the User class sooncopy
-	def deposit(self, amount):
-		# your code here
-	def withdraw(self, amount):
-		# your code here
-	def display_account_info(self):
-		# your code here
-	def yield_interest(self):
-		# your code 
+    def __init__(self, int_rate = 0.01, balance = 0):
+        self.int_rate = int_rate
+        self.balance = balance
+
+    def deposit(self, amount):
+        if amount < 0:
+            print("Cannot deposit that amount: $", amount)
+            return self
+        
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        new_balance = self.balance - amount
+
+        if new_balance <= 0:
+            print("Insufficient funds: Charging a $5 fee")
+            self.balance -= 5
+            return self
+        
+        #withdraw from the account
+        self.balance -= amount
+        return self
+
+    def display_account_info(self):
+        print(f"Account Balace: ${self.balance}, Interest Rate: {self.int_rate}%")
+        return self
+
+    def yield_interest(self):
+        self.balance += self.balance * self.int_rate
+        return self
+
+act1 = BankAccount(0, 1000)
+act1.deposit(1000).deposit(500).deposit(500).withdraw(100).yield_interest().display_account_info()
+
+act2 = BankAccount(1.25)
+act2.deposit(300).deposit(1).withdraw(100).withdraw(100).withdraw(100).withdraw(3).yield_interest().display_account_info()
