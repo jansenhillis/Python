d6ead387fcbee3c8402edc42d5675f31f67b804b
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-01 10:25:58 -0800

    Added alternate time algorithm

--- A018_django_time_display/time_display/views.py
+++ A018_django_time_display/time_display/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from time import gmtime, strftime
+from datetime import datetime, timezone
     
 def time_display(request):
     context = {
-        "time": strftime("%Y-%m-%d %H:%M %p", gmtime())
+        "time": strftime("%Y-%m-%d %H:%M %p", gmtime()),
+        "time2" : datetime.now(timezone.utc),
     }
     return render(request,'index.html', context)
