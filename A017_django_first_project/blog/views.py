9650559e60f0480d33e152d6ef8862e86fa77d58
Jansen Hillis <jansen.hillis@outlook.com>
2021-01-31 18:16:55 -0800

    updated based on solution.

--- A017_django_first_project/blog/views.py
+++ A017_django_first_project/blog/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
     return HttpResponse("Placeholder to later display a list of blogs.")
+
+def create(request):
+    return redirect('/')
 
 def new(request):
     return HttpResponse("Placeholder to display a new form to createa a new blog.") 
@@ -13,4 +16,4 @@ def edit(request, number):
     return HttpResponse("Placeholder to edit blog number: " + str(number))
 
 def destroy(request, number):
-    return HttpResponse("Placeholder to delete blog number: " + str(number))
+    return redirect('/')
