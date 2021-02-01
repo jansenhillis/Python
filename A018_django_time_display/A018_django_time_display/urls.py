9ad2acae4cb625071cb4f1b142b9f3c28a8474cb
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-01 10:09:34 -0800

    Connected to custom stylesheet

--- A018_django_time_display/A018_django_time_display/urls.py
+++ A018_django_time_display/A018_django_time_display/urls.py
@@ -16,5 +16,5 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('time_display.urls'),
+    path('', include('time_display.urls')),
 ]
--- A018_django_time_display/time_display/urls.py
+++ A018_django_time_display/time_display/urls.py
@@ -1,7 +1,7 @@
-from django.shortcuts import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path = ('', views.time_display),
-    path = ('time_display', views.time_display),
+    path('', views.time_display),
+    path('time_display', views.time_display),
 ]
