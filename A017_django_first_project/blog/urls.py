f4248ba8285404beda54ad0ab3ae833968b16389
Jansen Hillis <jansen.hillis@outlook.com>
2021-01-31 18:17:58 -0800

    fixed create() redirect

--- A017_django_first_project/blog/urls.py
+++ A017_django_first_project/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('new', views.new),
-    path('create', views.index),
+    path('create', views.create),
     path('<int:number>', views.show),
     path('<int:number>/edit', views.edit),
     path('<int:number>/delete', views.destroy),
