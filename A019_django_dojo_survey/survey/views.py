6cc42776fd00ae33078f97b205c151459bf26bd2
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-02 11:09:27 -0800

    both pages are working now

--- A019_django_dojo_survey/survey/views.py
+++ A019_django_dojo_survey/survey/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
     return render(request, 'index.html')
 
 def result(request):
-    return HttpResponse(request)
+    if request.GET:
+        return redirect('/')
+    elif request.POST:
+        context = {
+            "name": request.POST["name"],
+            "locations": request.POST["locations"],
+            "lang": request.POST["lang"],
+            "description" : request.POST["description"],
+        }
+
+        return render(request, 'result.html', context)
+
