2eb94d9cf906022cd0c51f98c5894444e6efdb31
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-03 10:48:05 -0800

    connected a counter to the session

--- A021_django_random_num_gen/random_word/urls.py
+++ A021_django_random_num_gen/random_word/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('count', views.count),
+    path('/count', views.count),
 ]
--- A021_django_random_num_gen/random_word/views.py
+++ A021_django_random_num_gen/random_word/views.py
@@ -5,7 +5,11 @@ def index(request):
     
 
 def count(request):
-    print("count++")
+    # if the key doesn't exist, this is first run. Create it
+    if 'count' not in request.session.keys():
+        request.session['count'] = 0
+    
+    request.session['count'] += 1
     return render(request, 'index.html')
     
 
