0ecc4f051ff862fe984eb28c9b1e7a2b712b5f79
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-02 19:45:39 -0800

    Fixed routing issues. Should redirect properly now!

--- A020_django_dojo_survey_revist/survey/urls.py
+++ A020_django_dojo_survey_revist/survey/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('result', views.process),
+    path('submit', views.submit),
+    path('result', views.result)
 ]
--- A020_django_dojo_survey_revist/survey/views.py
+++ A020_django_dojo_survey_revist/survey/views.py
@@ -3,17 +3,16 @@ from django.shortcuts import render, HttpResponse, redirect
 def index(request):
     return render(request, 'index.html')
 
-def process(request):
+def submit(request):
     if request.method == "POST":
         request.session["name"] = request.POST["name"]
         request.session["locations"] = request.POST["locations"]
         request.session["lang"] = request.POST["lang"]
         request.session["description"] = request.POST["description"]
     
-        # return redirect(result(request))
-        # result(request)
-    
-    return redirect('/')
+        return redirect('/result')
+    else:
+        return redirect('/')
 
 def result(request):
-    return render(request, "result.html")
+    return render(request, 'result.html')
