a072041570502f96e58c29faab63b6fec00de7ca
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-03 11:29:53 -0800

    added random word generator, and reset methods

--- A021_django_random_num_gen/random_word/urls.py
+++ A021_django_random_num_gen/random_word/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('random_word', views.random_word),
+    path('random_word/reset', views.reset),
 ]
--- A021_django_random_num_gen/random_word/views.py
+++ A021_django_random_num_gen/random_word/views.py
@@ -1,16 +1,36 @@
 from django.shortcuts import render, redirect
+from django.utils.crypto import get_random_string
 
 def index(request):
+    for item in request.session.keys():
+        print(item)
     return redirect('/random_word')
     
 def random_word(request):
-    # if the key doesn't exist, this is first run. Create it
+    # If the key doesn't exist, this is first run. 
+    # init counter and word keys
     if 'count' not in request.session.keys():
         request.session['count'] = 0
+        request.session['random_word'] = '0123456789ABCD'
 
-    # get new random word
+    # load a new random word into the session
+    request.session['random_word'] = get_random_str()
 
     # increment counter 
     request.session['count'] += 1
 
     return render(request, 'index.html')
+
+# Source a random 14 char string
+def get_random_str():
+    return get_random_string(length=14)
+
+# Reset method to reset both counters to initial values
+def reset(request):
+    if 'count' in request.session.keys():
+        request.session['count'] = 0
+
+    if 'random_word' in request.session.keys():
+        request.session['random_word'] = '0123456789ABCD'
+
+    return redirect('/')
