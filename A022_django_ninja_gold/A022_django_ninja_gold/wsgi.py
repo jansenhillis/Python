514b316aa727fdf3aad84c9675795a0ffb14d756
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-03 13:07:34 -0800

    initial commit

---
+++ A022_django_ninja_gold/A022_django_ninja_gold/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for A022_django_ninja_gold project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'A022_django_ninja_gold.settings')
+
+application = get_wsgi_application()
