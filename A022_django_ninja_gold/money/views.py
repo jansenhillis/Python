945f5579c147b4afaf30f0f0a9337bc4ccce7de1
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-03 16:25:38 -0800

    gold counter in session implemented, with brute force version of POST lookup

--- A022_django_ninja_gold/money/views.py
+++ A022_django_ninja_gold/money/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render, redirect
 
+forms = {
+    'farm': { 'min': 10, 'max': 20 },
+    'cave': { 'min': 5, 'max': 10 },
+    'house': { 'min': 2, 'max': 5 },
+    'casino': { 'min': -50, 'max': 50 },
+}
+
 def index(request):
+    if 'owned_gold' not in request.session.keys():
+        request.session['owned_gold'] = 0
+        request.session['activities'] = []
     return render(request, 'index.html')
 
 def process_money(request):
-    return redirect('/')
+    if request.method == 'POST':
+        
+        # min/max values for each form
+        values = {}
+        if 'farm' in request.POST:
+            values = forms['farm']
+        elif 'cave' in request.POST:
+            values = forms['cave']
+        elif 'house' in request.POST:
+            values = forms['house']
+        elif 'casino' in request.POST:
+            values = forms['casino']
+
+        delta = get_random_number_between(values['min'], values['max'])
+        request.session['owned_gold'] += delta
+        return redirect('/')
+    else:
+        return redirect('/')
+
+
+def get_random_number_between(min=0, max=10):
+    return 5
