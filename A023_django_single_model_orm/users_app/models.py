1e66fba642748aebf0f981959ca9fee639615d47
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-05 14:59:00 -0800

    added User model

--- A023_django_single_model_orm/users_app/models.py
+++ A023_django_single_model_orm/users_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    # automatically includes id, updated_at, created_at
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.CharField(max_length=255)
+    age = models.IntegerField()
+
