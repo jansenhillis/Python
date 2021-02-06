a317e1cfc13835b823307b3db7420237250f89d7
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-05 22:34:11 -0800

    hooked up form to the databas, now writes and redirects

--- A024_django_users_with_templates/users_app/urls.py
+++ A024_django_users_with_templates/users_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('add_user', views.add_user),
 ]
--- A024_django_users_with_templates/users_app/views.py
+++ A024_django_users_with_templates/users_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Users
 
 def index(request):
@@ -7,3 +7,16 @@ def index(request):
     }
 
     return render(request, 'index.html', context)
+
+def add_user(request):
+    if request.method == 'POST':
+        first = request.POST['first_name'],
+        last = request.POST['last_name'],
+        email = request.POST['email_address'],
+        age = request.POST['age']  
+        
+        Users.objects.create(first_name=first[0], last_name=last[0], email_address=email[0], age=age[0])        
+
+        return redirect('/')
+    else:
+        return redirect('/')
