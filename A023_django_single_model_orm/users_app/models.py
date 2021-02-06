9de9c3668479b6f55f89d884a7196f2e48140c63
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-05 19:36:37 -0800

    errored out. removed.

--- A023_django_single_model_orm/users_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    # automatically includes id, updated_at, created_at
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email_address = models.CharField(max_length=255)
-    age = models.IntegerField()
-
