54c12fe73aac335bf8d1708db88c9a95a606a851
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-08 11:48:40 -0800

    initial commit

---
+++ A025_django_dojos_ninjas/A025_django_dojos_ninjas/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for A025_django_dojos_ninjas project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'A025_django_dojos_ninjas.settings')
+
+application = get_wsgi_application()
