132e30a71f5300e5e66d7a6a30e02d56ab8808cf
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-08 19:26:45 -0800

    assignment complete

--- A025_django_dojos_ninjas/A025_django_dojos_ninjas/settings.py
+++ A025_django_dojos_ninjas/A025_django_dojos_ninjas/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dojos_ninjas_app',
 ]
 
 MIDDLEWARE = [
--- A025_django_dojos_ninjas/dojos_ninjas_app/models.py
+++ A025_django_dojos_ninjas/dojos_ninjas_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Dojos(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    desc = models.TextField(blank=True)
+
+    def __repr__(self):
+        return f"Dojo: {self.name} ({self.city}, {self.state})"
+
+class Ninjas(models.Model):
+    dojo = models.ForeignKey(Dojos, related_name='ninjas', on_delete="models.PROTECT")
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __repr__(self):
+        return f"Ninja: {self.first_name} {self.last_name} '{self.dojo.name}'"
