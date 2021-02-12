b3de3569602b6129ae2ec3286b49e8561a76a905
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-11 16:40:58 -0800

    final set of commands

--- A027_django_book_authors/books_authors_app/models.py
+++ A027_django_book_authors/books_authors_app/models.py
@@ -11,6 +11,7 @@ class Authors(models.Model):
     first_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
     notes = models.TextField(blank=True)
+    books = models.ManyToManyField(Books, related_name='authors')
 
     def __repr__(self):
         return f" {self.id} {self.first_name} {self.last_name} "
