f44255f9527c7df12793be1495b8226548f39021
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-12 15:57:26 -0800

    Added model, bare index.html, populated database with stub values for testing

--- A028_django_semi_restful_tv_shows/shows_app/models.py
+++ A028_django_semi_restful_tv_shows/shows_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class TVShow(models.Model):
+    title = models.CharField(max_length=255)
+    network = models.CharField(max_length=45)
+    release_date = models.DateField()
+    description = models.TextField()
+
+    def __repr__(self):
+        return f"{self.title} {self.network}, ({self.release_date})"
--- A028_django_semi_restful_tv_shows/shows_app/views.py
+++ A028_django_semi_restful_tv_shows/shows_app/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, render
+from django.shortcuts import render, render, redirect
 
 def index(request):
+    return redirect('/shows')
+
+def shows(request):
     return render(request, 'index.html')
