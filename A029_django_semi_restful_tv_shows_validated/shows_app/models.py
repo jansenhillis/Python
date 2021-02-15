450a7ce90f5532ed21191098a66f41229e0cd999
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-15 13:54:37 -0800

    initial commit copy of A028

---
+++ A029_django_semi_restful_tv_shows_validated/shows_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class TVShow(models.Model):
+    title = models.CharField(max_length=255)
+    network = models.CharField(max_length=45)
+    release_date = models.DateField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return f"{self.title} {self.network}, ({self.release_date})"
---
+++ A029_django_semi_restful_tv_shows_validated/shows_app/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render, redirect
+from .models import TVShow
+from datetime import datetime
+
+def index(request):
+    return redirect('/shows')
+
+def shows(request):
+    context = {
+        "tvshows": TVShow.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def show_description(request, show_id):
+    # lookup show
+    show = TVShow.objects.get(id=show_id)
+
+    # populate context with show object
+    return render(request, 'show.html', {
+        "show": show
+    })
+
+def new(request):
+    return render(request, 'add.html')
+
+def create(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        network = request.POST['network']
+        release_date = request.POST['release_date']
+        description = request.POST['description']
+
+        show = TVShow.objects.create(title=title, network=network, release_date=release_date, description=description)
+
+        if show:
+            return redirect('show_details', show.id)
+        else:
+            return redirect('/')
+    else:
+        return redirect('/')
+
+def edit(request, show_id):
+    show = TVShow.objects.get(id=show_id)
+
+    # populate context with show object values
+    return render(request, 'edit.html', {
+        "show_id": show.id,
+        "show_title": show.title,
+        "show_network": show.network,
+        "show_release_date": show.release_date.strftime("%Y-%m-%d"),
+        "show_description": show.description,
+    })
+
+def update(request, show_id):
+    if request.method == 'POST':
+        title = request.POST['title']
+        network = request.POST['network']
+        release_date = request.POST['release_date']
+        description = request.POST['description']
+
+        show = TVShow.objects.get(id=show_id)
+
+        if show:
+            show.title = title
+            show.network = network
+            show.release_date = release_date
+            show.description = description 
+            show.save()
+
+            return redirect('show_details', show_id)
+        else:
+            return redirect('show_details', show_id)
+    else:
+        return redirect('show_details', show_id)
+
+def destroy(request, show_id):
+    show = TVShow.objects.get(id=show_id)
+
+    if show:
+        show.delete()
+    
+    return redirect('/shows')
