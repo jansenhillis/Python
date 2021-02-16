4e64979e44f2edf446ea0ff93dbb81caef629805
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-15 16:42:27 -0800

    Added checking the database before creating to find possible duplicates, and alphanum check on title

--- A029_django_semi_restful_tv_shows_validated/shows_app/models.py
+++ A029_django_semi_restful_tv_shows_validated/shows_app/models.py
@@ -7,13 +7,15 @@ class TVShowManager(models.Manager):
 
         # Validate size/presence of input
         if len(postData['title']) < 2:
-            errors['title'] = "Movie title is required. Should be at least 2 characters."
+            errors['title'] = "Show title is required. Should be at least 2 characters."
         if len(postData['network']) < 3:
             errors['network'] = "Network is required. Should be at least 3 characters."
         if len(postData['description']) < 10 and len(postData['description']) > 0:
             errors['description'] = "Description is optional, but requires at least 10 characters."
         
         # Validate alphanumerics
+        if not postData['title'].isalnum():
+            errors['title'] = "Show title can only contain alphanumeric characters"
 
         # Validate release_date is in the past, ignoring edge case of adding a new show the day it released
         if len(postData['release_date']) < 1:
@@ -27,7 +29,11 @@ class TVShowManager(models.Manager):
                 errors['release_date'] = "Release Date must be in the past."
 
         # Validate that a show doesn't already exist before creation
-
+        result = TVShow.objects.filter(title=postData['title'])
+        if result:
+            print(result)
+            errors['title'] = f"The movie '{ postData['title'] }'' already exists."
+            
         return errors
 
 class TVShow(models.Model):
