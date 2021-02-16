332720e416fb8d50d25f6ad1bc8582efce8619f7
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-15 22:03:45 -0800

    added start of models

--- A030_django_courses/course_app/models.py
+++ A030_django_courses/course_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class CourseManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+
+        return errors
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.TextField()
+    objects = CourseManager()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
