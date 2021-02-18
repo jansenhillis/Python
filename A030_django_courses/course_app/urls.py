c7ebee4020e02403472d79ee53ea907a1f252ac1
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-17 17:34:08 -0800

    added two routes, all html elements, initial add functionality

--- A030_django_courses/course_app/urls.py
+++ A030_django_courses/course_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('/add_course', views.add_course),
+    path('/courses/destroy/<int:course_id>', views.destroy, name="destroy")
 ]
