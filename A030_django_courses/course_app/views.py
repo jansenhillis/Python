706d0ebe1235d621597521fcf5ef9c0a42687bb4
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-17 17:54:02 -0800

    Added index page updates, new view for add_course, placeholder for destroy

--- A030_django_courses/course_app/views.py
+++ A030_django_courses/course_app/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Course
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {
+        "courses": Course.objects.all()
+    })
+
+def add_course(request):
+    if request.method == "POST":
+        errors = Course.objects.basic_validator(request.POST)
+        
+        if errors:
+            for key, value in errors.items():
+                messages.error(request, value)
+
+            return redirect('/')
+        else:
+            name = request.POST['name']
+            desc = request.POST['desc']
+            course = Course.objects.create(name=name, desc=desc)            
+            
+            return redirect('/')
+            
+def destroy(request, course_id):
+    pass
