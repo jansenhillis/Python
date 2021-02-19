8cfa8f166ac089eb8b48c8b749bdfdc3c9f46beb
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-18 23:10:19 -0800

    move to messages for sharing values around.
    Still need to implement fix for embedded form prices

--- A031_django_amadon/poorly_coded_store/urls.py
+++ A031_django_amadon/poorly_coded_store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('checkout/', views.checkout)
+    path('checkout/', views.checkout),
+    path('confirm', views.confirm)
 ]
--- A031_django_amadon/poorly_coded_store/views.py
+++ A031_django_amadon/poorly_coded_store/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
+from django.contrib import messages
 
 def index(request):
     context = {
@@ -12,15 +13,18 @@ def checkout(request):
     price_from_form = float(request.POST["price"])
     total_charge = quantity_from_form * price_from_form
     
-    print("Charging credit card...")
+    print(f"Charging credit card... {total_charge}")
     order = Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    
+    messages.info(request, f"We charged your credit card for ${ order.total_price }")
+
+    return redirect('/confirm')
+
+def confirm(request):
     net_spend = 0
     for order in Order.objects.all():
         net_spend += order.total_price
 
-    return render(request, "store/checkout.html", {
-        "order": order,
-        "total_ordered": Order.objects.all().count(),
-        "net_spend": net_spend
-    })
+    messages.info(request, f"You have ordered { Order.objects.all().count() } \
+        items so far and spent ${ net_spend } with Amadon.com!")
+
+    return render(request, "store/checkout.html")
