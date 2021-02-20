b84ee3896a4de64e68008ce83b3fcae508aac6ce
Jansen Hillis <jansen.hillis@outlook.com>
2021-02-19 22:08:12 -0800

    Added validation, tweaked login to remove uneeded checks

--- A032_django_login_registration/login_registration/views.py
+++ A032_django_login_registration/login_registration/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from .models import User
-import bcrypt
 from django.contrib import messages
 
 def index(request):
@@ -15,48 +14,25 @@ def register_view(request):
                 messages.error(request, error)
             return redirect('/')
         else:
-            # verify if user already exists
-            user = User.objects.filter(email=request.POST['reg_email'])
-            if not user:
-                first = request.POST['first_name']
-                last = request.POST['last_name']
-                email = request.POST['reg_email']
-                pw_hashed = bcrypt.hashpw(request.POST['reg_pw'].encode(), bcrypt.gensalt()).decode()
-
-                user = User.objects.create(first=first, last=last, email=email, pw=pw_hashed)
-
+            user = User.objects.register_user(request)
+            if user:
                 request.session['user_id'] = user.id
                 return redirect('success')
             else: # existing user found
-                messages.error(request, "Username already exists. Please try again.")
+                messages.error(request, "Username/email already exists. Please try again.")
                 return redirect('/')
 
 def login_view(request):
     if request.method == "POST":
-        errors = User.objects.validator(request.POST)
-
-        if errors:
-            for error in errors:
-                messages.error(request, error)
-            return redirect('/')
+        
+        user = User.objects.login_user(request)
+        if user:
+            request.session['user_id'] = user[0].id
+            return redirect('success')
         else:
-            # verify if user exists
-            user = User.objects.filter(email=request.POST['login_email'])
-            if user:
-                # verify password hashes match
-                if bcrypt.checkpw(request.POST['login_pw'].encode(), user[0].pw.encode()):
-                    # matched, drop the session variable
-                    request.session['user_id'] = user[0].id
-                    return redirect('success')
-                else:
-                    messages.error(request, "Password incorrect. Try again.")
-                    return redirect('/')
-            else:
-                messages.error(request, "User email not found.")
-                return redirect('/')
-
-
-
+            messages.error(request, "Email/Password incorrect. Try again.")
+            return redirect('/')
+            
 def logout(request):
     del request.session['user_id']
     return redirect('/')
